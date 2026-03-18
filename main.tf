bde6721cbb1767b69b6142263810552a5a45ab29
Abhay Singh <abhay97ps@gmail.com>
2026-03-18 16:13:29 -0400

    build: cloudfront dist

--- main.tf
+++ main.tf
@@ -64,7 +64,90 @@ resource "aws_s3_bucket_website_configuration" "webpage" {
 
 }
 
+# cloud front distribution
+resource "aws_cloudfront_origin_access_control" "current" {
+  name                              = "${local.s3_frontend_name}-oac"
+  origin_access_control_origin_type = "s3"
+  signing_behavior                  = "always"
+  signing_protocol                  = "sigv4"
+}
+
+resource "aws_cloudfront_distribution" "s3_distribution" {
+  depends_on          = [aws_s3_bucket.webpage]
+  default_root_object = "index.html"
+
+  origin {
+    domain_name              = aws_s3_bucket.webpage.bucket_regional_domain_name
+    origin_id                = "s3-${aws_s3_bucket.webpage.id}"
+    origin_access_control_id = aws_cloudfront_origin_access_control.current.id
+  }
+  comment = "Chatlab client distribution"
+  enabled = true
+  default_cache_behavior {
+    allowed_methods        = ["GET", "HEAD"]
+    cached_methods         = ["GET", "HEAD"]
+    target_origin_id       = "s3-${aws_s3_bucket.webpage.id}"
+    viewer_protocol_policy = "redirect-to-https"
+    forwarded_values {
+      query_string = false
+      cookies {
+        forward = "none"
+      }
+    }
+  }
+  viewer_certificate {
+    cloudfront_default_certificate = true
+  }
+  restrictions {
+    geo_restriction {
+      restriction_type = "none"
+      locations        = []
+    }
+  }
+
+}
+
+# bucket policy to allow cloudfront access
+data "aws_iam_policy_document" "cloudfront" {
+  statement {
+    sid     = "AllowCloudFrontServicePrincipalReadOnly"
+    effect  = "Allow"
+    actions = ["s3:GetObject"]
+    resources = [
+      aws_s3_bucket.webpage.arn,
+      "${aws_s3_bucket.webpage.arn}/*"
+    ]
+    principals {
+      type        = "Service"
+      identifiers = ["cloudfront.amazonaws.com"]
+    }
+    condition {
+      test     = "StringEquals"
+      variable = "AWS:SourceArn"
+      values   = [aws_cloudfront_distribution.s3_distribution.arn]
+    }
+  }
+}
+
+resource "aws_s3_bucket_policy" "allow_cloudfront" {
+  bucket = aws_s3_bucket.webpage.id
+  policy = data.aws_iam_policy_document.cloudfront.json
+}
+
+# upload files to s3 bucket
+resource "aws_s3_object" "index_html" {
+  bucket       = aws_s3_bucket.webpage.id
+  key          = "index.html"
+  source       = "index.html"
+  etag         = filemd5("index.html")
+  content_type = "text/html; charset=utf-8"
+}
+
 # outputs
 output "bucket_name" {
   value = aws_s3_bucket.webpage.bucket
+}
+
+output "cloudfront_domain_name" {
+  value = aws_cloudfront_distribution.s3_distribution.domain_name
 }
